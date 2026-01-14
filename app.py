9121780c10c89d1fed3f4331ca8aaa6a33b8bda5
damjan-popic <damjan.popic@gmail.com>
2026-01-14 22:10:39 +0100

    Modified app

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 # app.py
 from fastapi import FastAPI
-from routers import nlp, thesaurus
+from routers import nlp, thesaurus, collocations
 from core.thesaurus import load_thesaurus
+from core.collocations import load_collocations
 
 app = FastAPI(
     title="LingHub",
-    description="Hub service for Slovene/Italian NLP, thesaurus, and other language resources.",
-    version="0.1.0",
+    description="Hub service for Slovene/Italian NLP, thesaurus, collocations, and other language resources.",
+    version="0.2.0",
 )
 
 
 @app.on_event("startup")
 async def startup_event():
-    # Eager loading of thesaurus into RAM
+    # Eager loading into RAM
     load_thesaurus()
+    load_collocations()
 
 
 @app.get("/")
@@ -23,4 +25,4 @@ def root():
 
 app.include_router(nlp.router, prefix="/nlp", tags=["nlp"])
 app.include_router(thesaurus.router, prefix="/thesaurus", tags=["thesaurus"])
-
+app.include_router(collocations.router, tags=["collocations"])
